806866a103c9cd8a542ddd6dd6e7ded843409099
kotasuke55 <uverock5@gmail.com>
2023-06-01 09:57:41 +0900

    Controllerの新規作成

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,5 +1,50 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.ItemForm;
+
+@Controller
+@RequestMapping("/item")
 public class ItemController {
 
+	@GetMapping
+	public String index() {
+		return "/item/index";
+	}
+
+	@GetMapping("toroku")
+    public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm) {
+        // 処理を追加
+        return "item/torokuPage";
+    }
+
+	@PostMapping("toroku")
+	public String toroku(ItemForm itemForm) {
+		// 処理を追加
+		return "redirect:/item";
+	}
+
+	@GetMapping("henshu/{id}")
+	public String henshuPage(@PathVariable Integer id, @ModelAttribute("ItemForm") ItemForm itemForm) {
+		// 処理を追加
+		return "item/henshuPage";
+	}
+
+	@PostMapping("henshu/{id}")
+	public String henshu(@PathVariable Integer id, @ModelAttribute("ItemForm") ItemForm itemForm) {
+		// 処理を追加
+		return "redirect:item";
+	}
+
+	@PostMapping("sakujo/{id}")
+	public String sakufo(@PathVariable Integer id) {
+		// 処理を追加
+		return "redirect:item";
+	}
 }
